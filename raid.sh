468bd306cab366307c9c7238a35d4ebb0910fe6d
Ryan Lauterbach <ryan@radianit.com>
2017-03-02 17:01:27 +0000

    initial raid commit

---
+++ raid.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+mkdir -p /root/sda9
+mount /dev/sda9 /mnt || exit 1
+rsync -a /mnt/* /root/sda9
+umount /dev/sda9
+Prepare the partition layout
+
+# delete sda9
+sgdisk /dev/sda --delete=9
+
+# create new /dev/sda9 partition (max size)
+START=`sgdisk /dev/sda -f`
+END=`sgdisk /dev/sda -E`
+sgdisk /dev/sda --new=9:$START:$END --type=9:fd00
+sleep 0.5
+partprobe /dev/sda
+
+# Remove partition table from sdb
+sgdisk --clear -g /dev/sdb || exit 1
+
+# create a partition sdb9 with the size of sda9
+PARTDATA=( $(sgdisk -i 9 /dev/sda | grep 'Partition size' || exit 1) )
+SECTORS=$(expr ${PARTDATA[2]} + 2048)
+sgdisk /dev/sdb -a 2048 --new=9:2048:$SECTORS --type=9:fd00 || exit 1
+sleep 0.5
+partprobe /dev/sdb
+
+# Create the RAID
+mdadm --create /dev/md0 --level=1 --raid-devices=2 /dev/sda9 /dev/sdb9
+mkfs.ext4 -I 128 -L ROOT /dev/md0
+
+# Copy the Data
+mount /dev/md0 /mnt
+"rsync -a /root/sda9/* /mnt" || exit 1
+umount /mnt
+
+# Wait until resync is finished
+watch -n 1 cat /proc/mdstat
